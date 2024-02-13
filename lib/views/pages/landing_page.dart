a1c9a8f63226984b18672a2f9a0b87a4d221fb1b
kareem abdeen <kareem.elshreef18@gmail.com>
2024-02-13 13:40:49 +0200

    {FIX LATER} : handling landing page later cause i handle navigation of the user by another way in main intial route

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,10 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../controllers/Providers/auth_controller_provider.dart';
+import '../../services/auth.dart';
+import 'auth_page.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //! ليه اكسسنا الاوس عن طريق البروفايدر هنا
+    final auth = Provider.of<AuthBase>(context);
+    //   return StreamBuilder(
+    //     stream: auth.authStateChanges,
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.active) {
+    //         final user = snapshot.data;
+    //         if (user == null) {
+    //           return const AuthPage();
+    //         }
+    //         return const HomePage();
+    //       }
+    //       // TODO: We will refactor this to make one component for loading
+    //       return const Scaffold(
+    //         //! Loading Screen
+    //         body: Center(
+    //           child: CircularProgressIndicator(), //! A progress indicator
+    //         ),
+    //       );
+    //     },
+    //   );
+    // }
+    return StreamBuilder<User?>(
+      stream: Auth.getInstance().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user == null) {
+            return ChangeNotifierProvider<AuthController>(
+              create: (_) => AuthController(authBase: auth),
+              child: const AuthPage(),
+            );
+          }
+        }
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
   }
 }
